10df7a331224065d54d8d62609e9c0185c093c37
nukeboi1 <username1023456789@gmail.com>
2025-09-24 23:06:50 +0300

    fixed missing error messages, changed sumenus from each having own var to a gingle one

--- src/main.c
+++ src/main.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
 
-int in_menu = 1;
-int in_purchase = 0;
-int in_return = 0;
-int in_report = 0;
-int is_on = 1;
+// Menu tracking
+int menu = 1;
 
 void print_purchase_submenu() {
-    printf("*******************\n");
-    printf("Purchases Submenu - Under Construction\n");
-    printf("*******************\n");
     printf("------------------------\n");
     printf("1. Go back to menu.\n");
     printf("------------------------\n");
 }
 
 void print_return_submenu() {
-    printf("*******************\n");
-    printf("Returns Submenu - Under Construction\n");
-    printf("*******************\n");
     printf("------------------------\n");
     printf("1. Go back to menu.\n");
     printf("------------------------\n");
 }
 
 void print_report_submenu() {
-    printf("*******************\n");
-    printf("Report Submenu - Under Construction\n");
-    printf("*******************\n");
     printf("------------------------\n");
     printf("1. Go back to menu.\n");
     printf("------------------------\n");
 }
 
+// main menu
 void print_menu() {
     printf("------------------------\n");
     printf("1. See the purchases.\n");
@@ -42,39 +31,38 @@ void print_menu() {
     printf("------------------------\n");
 }
 
+// buffer clear function to fix bug with non-integer input
 void clear_buffer() {
     int buffer;
     while ((buffer = getchar()) != '\n' && buffer != EOF);
 }
 
 void main_loop() {
-    while (is_on == 1) {
-        while (in_menu == 1) {
+    while (menu != 0) {
+        while (menu == 1) {
             print_menu();
 
             printf("Your option: ");
             int option = 0;
 
+            // If input isn't integer - clears buffer; part of non-integer input bugfix
             if (scanf("%d", &option) != 1) {
                 printf("Invalid input. Valid options are 1-4!\n");
                 clear_buffer();
                 continue;
             }
-
+            // main menu options
             if (option == 1) {
-                in_menu = 0;
-                in_purchase = 1;
+                menu = 2;
             }
             else if (option == 2) {
-                in_menu = 0;
-                in_return = 1;
+                menu = 3;
             }
             else if (option == 3) {
-                in_menu = 0;
-                in_report = 1;
+                menu = 3;
             }
             else if (option == 4) {
-                is_on = 0;
+                menu = 0;
                 printf("Goodbye");
                 break;
             }
@@ -83,7 +71,8 @@ void main_loop() {
             }
         }
 
-        while (in_purchase == 1) {
+        // purchase submenu
+        while (menu == 2) {
             print_purchase_submenu();
             printf("Your option: ");
             int option = 0;
@@ -95,15 +84,15 @@ void main_loop() {
             }
 
             if (option == 1) {
-                in_menu = 1;
-                in_purchase = 0;
+                menu = 1;
             }
             else {
                 printf("Invalid input. The only valid option is 1!\n");
             }
         }
 
-        while (in_return == 1) {
+        // returns submenu
+        while (menu == 3) {
             print_return_submenu();
             printf("Your option: ");
             int option = 0;
@@ -115,11 +104,15 @@ void main_loop() {
             }
 
             if (option == 1) {
-                in_return = 0;
-                in_menu = 1;
+                menu = 1;
+            }
+            else {
+                printf("Invalid input. The only valid option is 1!\n");
             }
         }
-        while (in_report == 1) {
+
+        // report submenu
+        while (menu == 4) {
             print_report_submenu();
             printf("Your option: ");
             int option = 0;
@@ -131,12 +124,16 @@ void main_loop() {
             }
 
             if (option == 1) {
-                in_report = 0;
-                in_menu = 1;
+                menu = 1;
+            }
+            else {
+                printf("Invalid input. The only valid option is 1!\n");
             }
         }
     }
 }
+
+// goes to main loop, returns 0 if loop is broken with input "4" in main menu
 int main(void) {
     main_loop();
     return 0;
