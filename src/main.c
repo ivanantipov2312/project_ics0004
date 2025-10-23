a1d0812a32cfb1b80bb448e291077f672a2dc889
ivanantipov2312 <ivanantipov231@gmail.com>
2025-10-24 00:05:45 +0300

    simplify project structure

--- src/main.c
+++ src/main.c
@@ -2,35 +2,7 @@
 // use true to distinguish true value from integer literal 1
 #include <stdbool.h>
 
-// subemnus
-void print_purchase_submenu() {
-    printf("--------PURCHASES-------\n");
-    printf("1. Go back to menu.\n");
-    printf("------------------------\n");
-}
-
-void print_return_submenu() {
-    printf("---------RETURNS--------\n");
-    printf("1. Go back to menu.\n");
-    printf("------------------------\n");
-}
-
-void print_report_submenu() {
-    printf("----------REPORT--------\n");
-    printf("1. Go back to menu.\n");
-    printf("------------------------\n");
-}
-
-// main menu
-void print_menu() {
-    printf("-----------MAIN---------\n");
-    printf("1. See the purchases.\n");
-    printf("2. See the returns.\n");
-    printf("3. Manage the current day report.\n");
-    printf("4. Quit.\n");
-    printf("------------------------\n");
-}
-
+// UTIL FUNCTIONS
 // TODO: get get_valid_option and clear_buffer functions to a separate io.h file
 // buffer clear function to check for an Invalid input and clear the whole line if the input is Invalid
 void clear_buffer() {
@@ -42,7 +14,6 @@ void clear_buffer() {
 int get_valid_option(int option_min, int option_max) {
     int option;
     while (true) {
-        // TODO: make message a separate argument to the get_valid_option function
         printf("Your option: ");
 
         // If input is a non-integer or it's smaller than the minimum option or it's larger that the maximum option, try again
@@ -58,22 +29,80 @@ int get_valid_option(int option_min, int option_max) {
     return option;
 }
 
+// subemnus
+void purchase_submenu_process() {
+	while (true) {
+		printf("--------PURCHASES-------\n");
+		printf("1. Put a new purchase record.\n");
+		printf("2. Go back to menu.\n");
+		printf("------------------------\n");
+
+		int option = get_valid_option(1, 2);
+
+		if (option == 1) {
+
+		} else if (option == 2) {
+			break;
+		}
+	}
+}
+
+void return_submenu_process() {
+	while (true) {
+		printf("---------RETURNS--------\n");
+		printf("1. Put a new return record.\n");
+		printf("2. Go back to menu.\n");
+		printf("------------------------\n");
+
+		int option = get_valid_option(1, 2);
+		if (option == 1) {
+		} else if (option == 2) {
+			break;
+		}
+	}
+}
+
+void report_submenu_process() {
+	while (true) {
+		printf("----------REPORT--------\n");
+		printf("1. List all purchase records.\n");
+		printf("2. List all return records.\n");
+		printf("3. List all records.\n");
+		printf("4. Go back to menu.\n");
+		printf("------------------------\n");
+
+		int option = get_valid_option(1, 4);
+		if (option == 1) {
+
+		} else if (option == 2) {
+
+		} else if (option == 3) {
+
+		} else if (option == 4) {
+			break;
+		}
+	}
+}
+
 void main_loop() {
     while (true) {
-        print_menu();
+		printf("-----------MAIN---------\n");
+		printf("1. See the purchases.\n");
+		printf("2. See the returns.\n");
+		printf("3. Manage the current day report.\n");
+		printf("4. Quit.\n");
+		printf("------------------------\n");
+
         int option = get_valid_option(1, 4);
 
         // main menu options
         // Get the input until user hits 1 - "Return to the main menu"
         if (option == 1) {
-            print_purchase_submenu();
-            while ((option = get_valid_option(1, 1)) != 1);
+            purchase_submenu_process();
         } else if (option == 2) {
-            print_return_submenu();
-            while ((option = get_valid_option(1, 1)) != 1);
+            return_submenu_process();
         } else if (option == 3) {
-            print_report_submenu();
-            while ((option = get_valid_option(1, 1)) != 1);
+            report_submenu_process();
         } else if (option == 4) {
             printf("Goodbye!\n");
             break; // Quit the main loop
